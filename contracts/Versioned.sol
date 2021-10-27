3117909347f6b9358d4f49eed757d39da3d97768
boltsr <skrullweb.dev@gmail.com>
2021-10-27 11:53:05 -0400

    event test

--- contracts/Versioned.sol
+++ contracts/Versioned.sol
@@ -10,12 +10,12 @@ interface Version {
 
 
 contract Versioned is Ownable, Proxy {
-    event Upgrade(string version, address indexed predecessor, address indexed successor);
+    // event Upgrade(string version, address indexed predecessor, address indexed successor);
 
     // Update current version and store method configurations
     function setVersion(Version successor) public onlyOwner {
         address predecessor = delegate;
         delegate = address(successor);
-        emit Upgrade(successor.version(), predecessor, delegate);
+        // emit Upgrade(successor.version(), predecessor, delegate);
     }
 }
