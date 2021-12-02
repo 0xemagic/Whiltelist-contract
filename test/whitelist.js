35eda915261754b5184db79b5bf7774a59ec3cd4
boltsr <skrullweb.dev@gmail.com>
2021-12-02 08:07:22 -0500

    White list test

--- test/whitelist.js
+++ test/whitelist.js
@@ -7,9 +7,9 @@ contract("Whitelist", async ([addr1, addr2]) => {
     whitelist = await Whitelist.deployed();
   });
 
-  // it('should add address to whitelist', async () => {
-  //   await whitelist.add(addr1)
-  //   assert(await whitelist.isListed(addr1))
-  //   assert(!await whitelist.isListed(addr2))
-  // })
+  it('should add address to whitelist', async () => {
+    await whitelist.add(addr1)
+    assert(await whitelist.isListed(addr1))
+    assert(!await whitelist.isListed(addr2))
+  })
 });
